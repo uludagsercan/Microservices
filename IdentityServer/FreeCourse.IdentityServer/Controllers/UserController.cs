12ddaf27202f488e9bc46b73725411d6ac290391
Sercan Uludag <sercan.uludag91@hotmail.com>
2023-02-08 02:51:59 +0300

    Identity Server Servisi Eklendi.

---
+++ IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using FreeCourse.IdentityServer.Dtos;
+using FreeCourse.IdentityServer.Models;
+using FreeCourse.Shared.Dtos;
+using IdentityServer4;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace FreeCourse.IdentityServer.Controllers
+{
+    [Authorize(IdentityServerConstants.LocalApi.PolicyName)]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp([FromBody] SignupDto signup)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signup.Username,
+                Email = signup.Email,
+                City = signup.City,
+            };
+
+            var result = await _userManager.CreateAsync(user,signup.Password);
+            if (!result.Succeeded)
+            {
+                return BadRequest(ResponseDto<NoContent>.Fail(result.Errors.Select(x=> x.Description).ToList(),400));
+            }
+            return NoContent();
+        }
+    }
+}
