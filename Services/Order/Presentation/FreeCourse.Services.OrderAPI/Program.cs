240ca857ec2734e5f5c4f5a2209cd7708f1d0c23
Sercan Uludag <sercan.uludag91@hotmail.com>
2023-02-24 06:04:36 +0300

    OrderAPI Identityserver Authentication

--- Services/Order/Presentation/FreeCourse.Services.OrderAPI/Program.cs
+++ Services/Order/Presentation/FreeCourse.Services.OrderAPI/Program.cs
@@ -1,10 +1,23 @@
 using FreeCourse.Services.Order.Application;
 using FreeCourse.Services.Order.Persistence;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using System.IdentityModel.Tokens.Jwt;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+builder.Services.AddAuthorization();
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.Authority = builder.Configuration["IdentityServerUrl"];
+    options.Audience = "resource_order";
+    options.RequireHttpsMetadata = false;
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -19,7 +32,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
